ee7e6f1924e2e48296ef1486e42e402b76cd8457
Nicolas Polomack <nicolas@polomack.eu>
2023-06-02 21:36:10 +0200

    chore: added MFA support for tests

--- tests/login.rs
+++ tests/login.rs
@@ -8,11 +8,12 @@ use std::env;
 async fn login_and_logout_test() {
     let email = env::var("MEGA_EMAIL").expect("missing MEGA_EMAIL environment variable");
     let password = env::var("MEGA_PASSWORD").expect("missing MEGA_PASSWORD environment variable");
+    let mfa = env::var("MEGA_MFA").ok();
 
     let http_client = reqwest::Client::new();
     let mut mega = mega::Client::builder().build(http_client).unwrap();
 
-    mega.login(&email, &password, None)
+    mega.login(&email, &password, mfa.as_deref())
         .await
         .expect("could not log in to MEGA");
 
